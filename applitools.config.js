3aeebf4f1857352a2bcffe98aa24710a583278ff
Zach Layman <zachlayman@Zachs-MacBook-Pro.local>
2019-03-26 11:44:32 -0700

    working cypress example

---
+++ applitools.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // appName: 'My app',
+  // showLogs: true,
+  // batchName: 'My batch'
+}
---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,17 @@
+describe('Hello world', () => {
+    it('works', () => {
+        cy.visit('https://applitools.com/helloworld');
+
+        cy.eyesOpen({
+            appName: 'Hello World!',
+            testName: 'My first JavaScript test!',
+            browser: { width: 800, height: 600 },
+        });
+
+        cy.eyesCheckWindow('Main Page');
+        cy.get('button').click();
+        cy.eyesCheckWindow('Click!');
+
+        cy.eyesClose();
+    });
+});
